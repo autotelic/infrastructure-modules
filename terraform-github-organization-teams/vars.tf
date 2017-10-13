6d222f76dae1c20042c68768a2e9b484c4d1c435
jkirkpatrick24 <jkirkpat16@gmail.com>
2017-10-13 10:41:45 -0700

    terraform module for creating githb teams

---
+++ terraform-github-organization-teams/vars.tf
@@ -0,0 +1,12 @@
+variable "team_name" {
+  description = "The name for the team"
+}
+
+variable "team_description" {
+  description = "The description for the team"
+}
+
+variable "team_privacy" {
+  description = "The privacy setting for the team"
+  default     = "closed"
+}
