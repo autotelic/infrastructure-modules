ed992660e009ea966fdd5a0d17d295172f418d1a
jkirkpatrick24 <jkirkpat16@gmail.com>
2017-11-02 14:21:50 -0700

    name module properly

--- examples/google-cloud-spanner-database/main.tf
+++ examples/google-cloud-spanner-database/main.tf
@@ -3,7 +3,7 @@ module "spanne_instance" {
   project_name = "demo-project"
 }
 
-module "name" {
+module "spanner_database" {
   source   = "github.com/stonevanzuiden/infrastructure-modules//google-spanner-database"
   instance = "${module.spanner_instance.instance_name}"
   name     = "demo-project"
