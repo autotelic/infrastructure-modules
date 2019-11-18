4d2052aa87afc8203521ab2778e20c8867411b2d
James Kirkpatrick <jkirkpat16@gmail.com>
2019-11-18 09:36:09 -0800

    Add public access controls (#10)

--- terraform-remote-state-s3-bucket/main.tf
+++ terraform-remote-state-s3-bucket/main.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 resource "aws_s3_bucket" "state_bucket" {
 
-  bucket = "${var.bucket_name}"
+  bucket = var.bucket_name
 
   versioning {
     enabled = true
@@ -12,4 +12,13 @@ resource "aws_s3_bucket" "state_bucket" {
     prevent_destroy = true
   }
 
+}
+
+resource "aws_s3_bucket_public_access_block" "remote_state_storage_storage_access_rules" {
+  bucket = aws_s3_bucket.state_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
