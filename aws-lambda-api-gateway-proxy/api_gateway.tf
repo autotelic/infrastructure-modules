10560a27dca540bba65a2c71d00505ef59b8e7cb
jkirkpatrick24 <jkirkpat16@gmail.com>
2020-01-16 15:53:58 -0800

    upgrade aws-lambda-api-gateway-proxy to terraform v0.12

--- aws-lambda-api-gateway-proxy/api_gateway.tf
+++ aws-lambda-api-gateway-proxy/api_gateway.tf
@@ -1,47 +1,47 @@
 resource "aws_api_gateway_rest_api" "api" {
-  name = "${var.api_gateway_name}"
+  name = var.api_gateway_name
 }
 
 resource "aws_api_gateway_resource" "resource" {
   path_part   = "{proxy+}"
-  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
 resource "aws_api_gateway_method" "method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
-  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  depends_on              = ["aws_api_gateway_method.method"]
-  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  resource_id             = "${aws_api_gateway_resource.resource.id}"
-  http_method             = "${aws_api_gateway_method.method.http_method}"
+  depends_on              = [aws_api_gateway_method.method]
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${aws_lambda_function.lambda.function_name}/invocations"
 }
 
-resource "aws_api_gateway_method_response" "200" {
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_resource.resource.id}"
-  http_method = "${aws_api_gateway_method.method.http_method}"
+resource "aws_api_gateway_method_response" "status_ok_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
   status_code = "200"
 }
 
 resource "aws_api_gateway_integration_response" "integration_response" {
   depends_on = [
-    "aws_api_gateway_method.method",
-    "aws_api_gateway_integration.integration",
+    aws_api_gateway_method.method,
+    aws_api_gateway_integration.integration,
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  resource_id = "${aws_api_gateway_resource.resource.id}"
-  http_method = "${aws_api_gateway_method.method.http_method}"
-  status_code = "${aws_api_gateway_method_response.200.status_code}"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = aws_api_gateway_method_response.status_ok_200.status_code
 
   response_templates = {
     "application/json" = ""
@@ -50,10 +50,11 @@ resource "aws_api_gateway_integration_response" "integration_response" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
-    "aws_api_gateway_method.method",
-    "aws_api_gateway_integration.integration",
+    aws_api_gateway_method.method,
+    aws_api_gateway_integration.integration,
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "${var.api_gateway_deployment_name}"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = var.api_gateway_deployment_name
 }
+
