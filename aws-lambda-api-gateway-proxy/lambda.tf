550adfb9c69ea2192f691e604d6866bbd3fbde21
James Kirkpatrick <jkirkpat16@gmail.com>
2020-01-16 16:41:43 -0800

    upgrade aws-lambda-api-gateway-proxy to terraform v0.12 (#11)

--- aws-lambda-api-gateway-proxy/lambda.tf
+++ aws-lambda-api-gateway-proxy/lambda.tf
@@ -2,18 +2,18 @@
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.lambda.arn}"
+  function_name = aws_lambda_function.lambda.arn
   principal     = "apigateway.amazonaws.com"
   source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 resource "aws_lambda_function" "lambda" {
-  filename         = "${data.archive_file.lambda_zip.output_path}"
-  function_name    = "mylambda"
-  role             = "${aws_iam_role.role.arn}"
+  filename         = data.archive_file.lambda_zip.output_path
+  function_name    = var.lambda_name
+  role             = aws_iam_role.role.arn
   handler          = "${var.lambda_name}.handler"
-  runtime          = "${var.runtime}"
-  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  runtime          = var.runtime
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
 
 # IAM role
@@ -35,4 +35,6 @@ resource "aws_iam_role" "role" {
   ]
 }
 POLICY
+
 }
+
--- aws-lambda-api-gateway-proxy/outputs.tf
+++ aws-lambda-api-gateway-proxy/outputs.tf
@@ -1,3 +1,4 @@
 output "base_url" {
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}"
+  value = aws_api_gateway_deployment.deployment.invoke_url
 }
+
