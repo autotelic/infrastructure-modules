f5d6288374ad7fe8a29a7276cba263cbff7dde19
Lian Yelich <lianyelich14@gmail.com>
2020-07-22 13:57:30 -0700

    Import resources from terraform-static-site-cloudfront-distribution

--- terraform-aws-static-site/vars.tf
+++ terraform-aws-static-site/vars.tf
@@ -6,3 +6,22 @@ variable "redirect_address" {
   description = "The address to redirect requests to"
 }
 
+variable "origin_id" {
+  description = "The unique ID for the cloudfront distribution"
+}
+
+variable "domain_name" {
+  description = "The domain name for the cloudfront distribution"
+}
+
+variable "aliases" {
+  description = "cNAMEs for the distribution"
+  type        = list(string)
+}
+
+variable "acm_arn" {
+  description = "the arn of the certificate"
+}
+
+
+
