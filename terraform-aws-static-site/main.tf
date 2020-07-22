7d085e70d68c0572b2a9ec35f5e5c52e8352e4d4
Lian Yelich <lianyelich14@gmail.com>
2020-07-22 13:57:30 -0700

    Import resources from terraform-static-site-cloudfront-distribution

--- terraform-aws-static-site/main.tf
+++ terraform-aws-static-site/main.tf
@@ -43,5 +43,65 @@ resource "aws_s3_bucket" "www-bucket" {
   }
 }
 
+# From /terraform-static-site-cloudfront-distribution
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = var.domain_name
+    origin_id   = var.origin_id
+
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port              = "80"
+      https_port             = "443"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+  }
+
+  price_class = "PriceClass_100"
+
+  enabled = true
+
+  aliases = var.aliases
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.origin_id
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = var.acm_arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = "0"
+    error_code            = "403"
+    response_code         = "200"
+    response_page_path    = "/index.html"
+  }
+}
+
+
 
 
