028be7ebd9c0ef8b0a6d2673ece10cfab485463c
Lian Yelich <lianyelich14@gmail.com>
2020-07-23 16:03:34 -0700

    Update vars.tf with new variables

--- terraform-aws-static-site/vars.tf
+++ terraform-aws-static-site/vars.tf
@@ -2,6 +2,10 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
 }
 
+variable "www_bucket_name" {
+  description = "The name of the www S3 bucket"
+}
+
 variable "redirect_address" {
   description = "The address to redirect requests to"
 }
@@ -17,12 +21,17 @@ variable "domain_name" {
 variable "aliases" {
   description = "cNAMEs for the distribution"
   type        = list(string)
-  default = "www.autotelic.com, www.autotelic.ca, autotelic.com, autotelic.ca"
 }
 
-variable "acm_arn" {
-  description = "the arn of the certificate"
+variable "www_origin_id" {
+  description = "The unique ID for the cloudfront distribution"
 }
 
+variable "www_domain_name" {
+  description = "The domain name for the cloudfront distribution"
+}
 
-
+variable "www_aliases" {
+  description = "cNAMEs for the distribution"
+  type        = list(string)
+}
