d571aaee7538957590302f332149be098aad918e
Lian Yelich <lianyelich14@gmail.com>
2020-07-24 13:34:59 -0700

    Enable cloudfront compression and increase time to live

--- terraform-aws-static-site/main.tf
+++ terraform-aws-static-site/main.tf
@@ -73,9 +73,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
+    compress               = true
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 31557600
+    max_ttl                = 31557600
   }
 
   viewer_certificate {
@@ -130,9 +131,10 @@ resource "aws_cloudfront_distribution" "www_s3_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
+    compress               = true
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 31557600
+    max_ttl                = 31557600
   }
 
   viewer_certificate {
